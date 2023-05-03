67f35f6c49f9f6e171b92eb475c9d2ef1f866a47
Bektur <prostreyd@gmail.com>
2023-05-03 13:26:15 +0600

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const cleanCSS = require("gulp-clean-css");
+const del = require("del");
+const pug = require("gulp-pug");
+const imagemin = require("gulp-imagemin");
+const uglify = require("gulp-uglify");
+
+// clean css style before updating style
+gulp.task("clean", function () {
+  return del(["dist/assets/style/main.css"]);
+});
+
+gulp.task("styles", () =>
+  gulp
+    .src("src/scss/main.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(cleanCSS({ compatibility: "ie11" }))
+    .pipe(gulp.dest("dist/assets/style"))
+);
+
+gulp.task("copy-img", function () {
+  return gulp.src("src/img/*").pipe(gulp.dest("dist/assets/img"));
+});
+
+// Minifying JS files
+gulp.task("script", () =>
+  gulp
+    .src("src/script/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("dist/assets/script"))
+);
+
+gulp.task("pug", () =>
+  gulp
+    .src("src/pug/index.pug")
+    .pipe(
+      pug({
+        pretty: true,
+      })
+    )
+    .pipe(gulp.dest("dist"))
+);
+
+gulp.task("watch", () => {
+  gulp.watch("src/scss/*.scss", (done) => {
+    gulp.series(["clean", "styles"])(done);
+  });
+  gulp.watch("src/img/*", (done) => {
+    gulp.series(["copy-img"])(done);
+  });
+  gulp.watch("src/script/*.js", (done) => {
+    gulp.series(["script"])(done);
+  });
+  gulp.watch("src/pug/*.pug", (done) => {
+    gulp.series(["pug"])(done);
+  });
+});
+
+gulp.task(
+  "default",
+  gulp.series(["clean", "styles", "copy-img", "script", "pug"])
+);
